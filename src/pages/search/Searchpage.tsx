04bff3b8fb640b5952136cd5663e8ed55a581bc7
AnneCode <anneliu49@gmail.com>
2021-09-25 16:31:07 -0400

    add two layouts and refact home,search and detail page

--- src/pages/search/Searchpage.tsx
+++ src/pages/search/Searchpage.tsx
@@ -6,7 +6,7 @@ import { Spin } from 'antd';
 import { useDispatch } from 'react-redux'
 import { useSelector } from '../../redux/hooks'
 import { searchProduct } from '../../redux/productSearch/slice'
-
+import { MainLayout } from '../../layouts/mainLayout'
 interface MatchParams {
   keywords: string;
 }
@@ -46,24 +46,19 @@ export const SearchPage: React.FC = () => {
     return <div>网站出错：{error}</div>;
   }
   return (
-    <>
-      <Header />
-      <div className={styles["page-content"]}>
-        {/* 分类过滤器 */}
-        <div className={styles["product-list-container"]}>
-          <FilterArea />
-        </div>
-        {/* 产品列表  */}
-        <div className={styles["product-list-container"]}>
-          <ProductList
-            data={productList}
-            paging={pagination}
-            onPageChange={onPageChange}
-          />
-
-        </div>
+    <MainLayout>
+      {/* 分类过滤器 */}
+      <div className={styles["product-list-container"]}>
+        <FilterArea />
       </div>
-      <Footer />
-    </>
+      {/* 产品列表  */}
+      <div className={styles["product-list-container"]}>
+        <ProductList
+          data={productList}
+          paging={pagination}
+          onPageChange={onPageChange}
+        />
+      </div>
+    </MainLayout>
   );
 };
