9dbae39b40842bfd52562be7494ee3f47e04a11d
AnneCode <anneliu49@gmail.com>
2021-10-09 17:41:31 -0400

    build priviate router in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import styles from "./App.module.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomePage, SignInPage, RegisterPage, DetailPage, SearchPage } from './pages';
+import { Redirect } from "react-router-dom";
+import { useSelector } from "./redux/hooks";
+import { useDispatch } from "react-redux";
+import { getShoppingCart } from "./redux/shoppingCart/slice";
+
+
+const PrivateRoute = ({ component, isAuthenticated, ...rest }) => {
+  const routeComponent = (props) => {
+    return isAuthenticated ? (
+      React.createElement(component, props)
+    ) : (
+      <Redirect to={{ pathname: "/signIn" }} />
+    );
+  }
+  return <Route render={routeComponent} {...rest} />;
+}
 function App() {
   return (
     <div className={styles.App}>
